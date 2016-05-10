e2086bc7bc4b66679654a59dad53b04f9267517b
Barry de Koning <dekoning68@hotmail.com>
2016-05-10 14:03:05 +0200

    volledige database aanmaking

---
+++ Voedselboekje/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Naam] NVARCHAR(50) NULL, 
+    [Koolhydraten] DECIMAL NULL, 
+    [Eiwit] DECIMAL NULL, 
+    [Vet] DECIMAL NULL, 
+    [Kilocalorieën] DECIMAL NULL
+)
